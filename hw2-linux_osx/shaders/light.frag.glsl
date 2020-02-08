29654963978f8f60b739ac086e00ba62dac88769
colorfulgreen <colorfulgreen@gmail.com>
2020-02-08 09:46:30 +0800

    init hw2

---
+++ hw2-linux_osx/shaders/light.frag.glsl
@@ -0,0 +1,52 @@
+# version 330 core
+// Do not use any version older than 330!
+
+/* This is the fragment shader for reading in a scene description, including 
+   lighting.  Uniform lights are specified from the main program, and used in 
+   the shader.  As well as the material parameters of the object.  */
+
+// Inputs to the fragment shader are the outputs of the same name of the vertex shader.
+// Note that the default output, gl_Position, is inaccessible!
+in vec3 mynormal; 
+in vec4 myvertex; 
+
+// You will certainly need this matrix for your lighting calculations
+uniform mat4 modelview;
+
+// This first defined output of type vec4 will be the fragment color
+out vec4 fragColor;
+
+uniform vec3 color;
+
+const int numLights = 10; 
+uniform bool enablelighting; // are we lighting at all (global).
+uniform vec4 lightposn[numLights]; // positions of lights 
+uniform vec4 lightcolor[numLights]; // colors of lights
+uniform int numused;               // number of lights used
+
+// Now, set the material parameters.
+// I use ambient, diffuse, specular, shininess. 
+// But, the ambient is just additive and doesn't multiply the lights.  
+
+uniform vec4 ambient; 
+uniform vec4 diffuse; 
+uniform vec4 specular; 
+uniform vec4 emission; 
+uniform float shininess; 
+
+void main (void) 
+{       
+    if (enablelighting) {       
+        vec4 finalcolor; 
+
+        // YOUR CODE FOR HW 2 HERE
+        // A key part is implementation of the fragment shader
+
+        // Color all pixels black for now, remove this in your implementation!
+        finalcolor = vec4(0.0f, 0.0f, 0.0f, 1.0f); 
+
+        fragColor = finalcolor; 
+    } else {
+        fragColor = vec4(color, 1.0f); 
+    }
+}
